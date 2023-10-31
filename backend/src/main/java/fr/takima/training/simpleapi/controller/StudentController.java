ae61dbe0d926dd445856de1f39ca55417f1b6e06
Émile <emile.royer@epfedu.fr>
2023-10-31 09:33:55 +0100

    Use a StudentDto to input students

--- backend/src/main/java/fr/takima/training/simpleapi/controller/StudentController.java
+++ backend/src/main/java/fr/takima/training/simpleapi/controller/StudentController.java
@@ -1,6 +1,8 @@
 package fr.takima.training.simpleapi.controller;
 
+import fr.takima.training.simpleapi.dto.StudentDto;
 import fr.takima.training.simpleapi.entity.Student;
+import fr.takima.training.simpleapi.service.DepartmentService;
 import fr.takima.training.simpleapi.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,54 +16,72 @@ import java.util.Optional;
 @RequestMapping(value = "/students")
 public class StudentController {
     private final StudentService studentService;
+    private final DepartmentService departmentService;
+
+    private final Student studentDtoToStudent(StudentDto studentDto) {
+        Student student = new Student();
+        student.setId(0L); // A DTO has no ID
+        student.setFirstname(studentDto.firstname());
+        student.setLastname(studentDto.lastname());
+        student.setDepartment(
+                departmentService.getDepartmentById(studentDto.departmentId()));
+        return student;
+    }
 
     @Autowired
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService,
+            DepartmentService departmentService) {
         this.studentService = studentService;
+        this.departmentService = departmentService;
     }
 
     @GetMapping(value = "/")
     public ResponseEntity<Object> getStudents() {
-        return  ResponseEntity.ok(studentService.getAll());
+        return ResponseEntity.ok(studentService.getAll());
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Object> getStudentById(@PathVariable(name="id") long id) {
+    public ResponseEntity<Object> getStudentById(@PathVariable(name = "id") long id) {
         Optional<Student> studentOptional = Optional.ofNullable(this.studentService.getStudentById(id));
         if (studentOptional.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
-        return  ResponseEntity.ok(studentOptional.get());
+        return ResponseEntity.ok(studentOptional.get());
     }
 
     @PostMapping
-    public ResponseEntity<Object> addStudent(@RequestBody Student student) {
+    public ResponseEntity<Object> addStudent(@RequestBody StudentDto studentDto) {
         Student savedStudent;
         try {
-            savedStudent = this.studentService.addStudent(student);
+            savedStudent = this.studentService.addStudent(
+                    this.studentDtoToStudent(studentDto));
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
         }
 
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-                .buildAndExpand(savedStudent.getId()).toUri();
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedStudent.getId())
+                .toUri();
         return ResponseEntity.created(location).build();
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<Object> updateStudent(@RequestBody Student student, @PathVariable(name="id") long id) {
+    public ResponseEntity<Object> updateStudent(@RequestBody StudentDto studentDto,
+            @PathVariable(name = "id") long id) {
         Optional<Student> studentOptional = Optional.ofNullable(studentService.getStudentById(id));
         if (studentOptional.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
 
+        Student student = this.studentDtoToStudent(studentDto);
         student.setId(id);
         this.studentService.addStudent(student);
         return ResponseEntity.ok(student);
     }
 
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Object> removeStudent(@PathVariable(name="id") long id) {
+    public ResponseEntity<Object> removeStudent(@PathVariable(name = "id") long id) {
         Optional<Student> studentOptional = Optional.ofNullable(studentService.getStudentById(id));
         if (studentOptional.isEmpty()) {
             return ResponseEntity.notFound().build();
--- backend/src/main/java/fr/takima/training/simpleapi/dao/DepartmentDAO.java
+++ backend/src/main/java/fr/takima/training/simpleapi/dao/DepartmentDAO.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DepartmentDAO extends JpaRepository<Department, Long> {
     Department findDepartmentByName(String name);
+
+    Department findDepartmentById(Long id);
 }
